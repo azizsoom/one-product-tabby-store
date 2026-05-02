5c49311d120baacfebec93f376e7353455edc292
azizsoom <131043395+azizsoom@users.noreply.github.com>
2026-05-02 20:49:21 +0300

    Fix Supabase environment variable access

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const urlName = 'NEXT_PUBLIC_' + 'SUPABASE_URL';
-const keyName = 'NEXT_PUBLIC_' + 'SUPABASE_ANON_KEY';
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-export const db = createClient(process.env[urlName] || '', process.env[keyName] || '');
+export const db = createClient(url, key);
